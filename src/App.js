412749ac46f29f7f4e2d96ec2b4cc66aaaa49351
Felipe Romao <feliperomaocad@gmail.com>
2019-01-17 10:48:28 -0300

    Adicionando dependencias do template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className="wrapper">
+        <h1>App</h1>
       </div>
     );
   }
